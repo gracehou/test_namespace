6d5304432fa1b7d858858e7c782cc7026a34f9e4
hou.sheng.qing@163.com <hsq8726>
2017-12-14 17:23:15 +0800

    add

---
+++ app/src/PRINTIT.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\src;
+/**
+ * Created by PhpStorm.
+ * User: grace
+ * Date: 17-12-14
+ * Time: 下午4:12
+ */
+class PRINTIT
+{
+
+    function doPrint()
+    {
+        echo 'hello world';
+    }
+}
---
+++ app/src/User.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\src;
+/**
+ * Created by PhpStorm.
+ * User: grace
+ * Date: 17-12-14
+ * Time: 下午2:37
+ */
+
+class User
+{
+    public function __construct()
+    {
+        echo 'User namespace!';
+    }
+
+
+}
